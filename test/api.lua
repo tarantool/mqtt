1d23e540d553fd13f94193bd99715c3539977deb
Alexander Turenko <alexander.turenko@tarantool.org>
2018-11-26 22:22:18 +0300

    test: fix check of return value in api tests
    Related to #19.

--- test/api.lua
+++ test/api.lua
@@ -41,7 +41,7 @@ local conn = mqtt.new()
 
 assert(type(conn.VERSION) == 'string')
 
-F(conn:on_connect(function(success, ret_code, message)
+F('on_connect', conn:on_connect(function(success, ret_code, message)
   if not success then
     print("can't connect msg = %s, ret_code = %d", message, ret_code)
     os.exit(1)
@@ -49,19 +49,19 @@ F(conn:on_connect(function(success, ret_code, message)
   stat_inc('on_connect')
 end))
 
-F(conn:on_publish(function(mid)
+F('on_publish', conn:on_publish(function(mid)
   stat_inc('on_publish')
 end))
 
-F(conn:on_subscribe(function(mid, QoS)
+F('on_subscribe', conn:on_subscribe(function(mid, QoS)
   stat_inc('on_subscribe')
 end))
 
-F(conn:on_unsubscribe(function(mid)
+F('on_unsubscribe', conn:on_unsubscribe(function(mid)
   stat_inc('on_unsubscribe')
 end))
 
-F(conn:on_disconnect(
+F('on_disconnect', conn:on_disconnect(
   function(success, ret_code, message)
     if not success then
       print("can't connect msg = %s, ret_code = %d", message, ret_code)
@@ -71,7 +71,7 @@ F(conn:on_disconnect(
   end
 ))
 
-F(conn:on_message(
+F('on_message', conn:on_message(
   function(mid, topic, payload, qos, retain)
     stat_inc('on_message')
     stat[topic .. '_' .. payload] = {mid=mid}
@@ -89,24 +89,24 @@ E('reconnect', conn:reconnect())
 
 -- Here is setup for defaul Rabbitmq MQTT provider.
 -- (!) Except. login_set [[
-F(conn:tls_insecure_set(false))
+F('tls_insecure_set', conn:tls_insecure_set(false))
 F('connect', conn:connect({host="127.0.0.1", port=1883}))
-F(conn:login_set('user', 'password'))
+F('login_set', conn:login_set('user', 'password'))
 -- ]]
 
 F('reconnect', conn:reconnect())
 
-F(conn:subscribe('some/topic'))
-F(conn:will_set('some/topic', 'str', 0, false))
-F(conn:will_clear())
-F(conn:unsubscribe('some/topic'))
+F('subscribe', conn:subscribe('some/topic'))
+F('will_set', conn:will_set('some/topic', 'str', 0, false))
+F('will_clear', conn:will_clear())
+F('unsubscribe', conn:unsubscribe('some/topic'))
 
-F(conn:subscribe('channel/#'))
-F(conn:publish('channel/in_1', 'data_1', 0, false))
-F(conn:publish('channel/in_1', 'data_2', 0, false))
-F(conn:publish('channel/in_2', 'data_1', 0, false))
-F(conn:publish('channel/in_2', 'data_2', 0, false))
-F(conn:unsubscribe('channel/#'))
+F('subscribe', conn:subscribe('channel/#'))
+F('publish', conn:publish('channel/in_1', 'data_1', 0, false))
+F('publish', conn:publish('channel/in_1', 'data_2', 0, false))
+F('publish', conn:publish('channel/in_2', 'data_1', 0, false))
+F('publish', conn:publish('channel/in_2', 'data_2', 0, false))
+F('unsubscribe', conn:unsubscribe('channel/#'))
 
 -- Join
 print ("[+] Join result")
@@ -122,7 +122,7 @@ assert(stat['channel/in_2_data_2'] ~= nil)
 
 print ("[+] Done")
 
-F(conn:destroy())
-F(conn:destroy())
+F('destroy', conn:destroy())
+F('destroy', conn:destroy())
 
 os.exit(0)
